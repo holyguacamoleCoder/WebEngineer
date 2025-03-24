9cb2f129ea798aa72613149abede2a053de62402
holyguacamoleCoder <2226192528@qq.com>
2025-03-24 17:59:34 +0800

    login page

--- frontend-web/src/router/index.js
+++ frontend-web/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '@/views/Login.vue'
+import Layout from '@/views/Layout.vue'
+import { checkLogin } from '@/api/login/login.js'
+import { getUserInfo } from '@/utils/storage.js'
+import routesConfig from '@/config/routes.json'
 
 Vue.use(VueRouter)
 
@@ -12,41 +16,48 @@ VueRouter.prototype.push = function push (location) {
 
 const router = new VueRouter({
   mode: "history",
-  routes:[
+  routes: [
     { 
       path: '/', 
       redirect: '/home'
     },
-    // { 
-    //   path: '/login', 
-    //   component: Login 
-    // },
-    // { 
-    //   path: '/layout', 
-    //   component: Layout, 
-    //   children: [
-       
-    //   ]
-    // }
-
-]
+    { 
+      path: '/login', 
+      component: Login 
+    },
+    { 
+      path: '/layout', 
+      component: Layout, 
+      children: []
+    }
+  ]
 })
 
-//配置游客可以访问的界面
+// 配置游客可以访问的界面
 const touristVisitUrl = ['/login']
 
-//全局路由前置守卫
-router.beforeEach( async (to, from, next) => {
+// 全局路由前置守卫
+router.beforeEach(async (to, from, next) => {
   if (touristVisitUrl.includes(to.path)) {
     next()
   } else {
-    // const userInfo = getUserInfo()
-    // const result = await checkLogin(userInfo)
-    // if (result.data.code == 0) {
-    //   next('/login')
-    //   Vue.prototype.$message.error('登录超时，请重新登录！')
-    // }
-    // next()
+    const userInfo = getUserInfo()
+    const result = await checkLogin(userInfo)
+    if (result.data.code == 0) {
+      next('/login')
+      Vue.prototype.$message.error('登录超时，请重新登录！')
+    } else {
+      // 动态添加路由
+      if (!router.hasRoute('dynamicRoutes')) {
+        const dynamicRoutes = routesConfig.routes
+        dynamicRoutes.forEach(route => {
+          const component = () => import(`@/views/${route.component}.vue`)
+          router.addRoute('layout', { ...route, component })
+        })
+        router.addRoute({ name: 'dynamicRoutes', path: '*', redirect: '/home' })
+      }
+      next()
+    }
   }
 })
 
--- frontend-web/src/utils/element-ui.js
+++ frontend-web/src/utils/element-ui.js
@@ -1,10 +1,22 @@
-import { 
-    Tag
+import {
+    Container, 
+    Header,
+    Aside,
+    Main, 
+    Tag,
+    Form,
+    FormItem,
 } from 'element-ui'
 
 const element = {
     install: function(Vue) {
+        Vue.use(Container)
+        Vue.use(Header)
+        Vue.use(Aside)
+        Vue.use(Main)
         Vue.use(Tag)
+        Vue.use(Form)
+        Vue.use(FormItem)
     }
 }
 
