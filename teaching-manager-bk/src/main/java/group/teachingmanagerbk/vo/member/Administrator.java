7cd01d3c603bc40656123a0b5bde5478e89b16fd
Flynn <162332331+userFlynn@users.noreply.github.com>
2025-06-10 18:04:42 +0800

    Delete

--- teaching-manager-bk/src/main/java/group/teachingmanagerbk/vo/member/Administrator.java
+++
@@ -1,9 +0,0 @@
-package group.teachingmanagerbk.vo.member;
-
-import lombok.Data;
-
-@Data
-public class Administrator {
-    private String administratorId;      //管理员id
-    private String name;    //管理员姓名
-}
